ed05aab26a0aa9e9e7b9d692c6ced911853d1c51
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-01-29 14:01:04 -0300

    feat: adicionar novas informações
    docs: alterar documentação

---
+++ Aula 02/app.js
@@ -0,0 +1,15 @@
+/**************************************************************
+* Objetivo: Projeto para realizar o cálculo de médias escolares
+* Autor: Gustavo Vidal de Abreu
+* Data: 29/01/2026 (quinta-feira)
+* Versão: 1.0
+**************************************************************/
+
+/*
+    Tipos de criação de variáveis:
+        - var   -> Permite criar um espaço em memória do tipo variável. Essa forma de criação, hoje, é considerada mais antiga e, provável, que seja encontrada apenas em projetos mais antigos. Caso precise utilizá-lo, recomenda-se utilizar somente em escopo global.
+
+        - let   -> 
+
+        - const ->
+*/
