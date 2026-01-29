765d6afe7e22352b8767cc402d8073e77f3682c5
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-01-29 14:18:20 -0300

    feat: adicionar últimas informações

--- Aula 02/app.js
+++ Aula 02/app.js
@@ -9,7 +9,7 @@
     Tipos de criação de variáveis:
         - var   -> Permite criar um espaço em memória do tipo variável. Essa forma de criação, hoje, é considerada mais antiga e, provável, que seja encontrada apenas em projetos mais antigos. Caso precise utilizá-lo, recomenda-se utilizar somente em escopo global.
 
-        - let   -> 
+        - let   -> Permite criar um espaço em memória do tipo variável. Essa forma de criação é realizada somente no escopo local, ou seja, dentro de bloco de programação {}. O mesmo deixa de existir ao término do bloco.
 
-        - const ->
+        - const -> Permite criar um espaço em memória do tipo constante. Essa forma de criação não poderá sofrer mudanças durante o projeto. Uma prática comum é identificá-la com LETRAS MAIÚSCULAS para facilitar sua localização.
 */
