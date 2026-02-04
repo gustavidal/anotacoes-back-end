83d8307190a4136bd7fba782ebe5f38c652f1fa7
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-04 17:01:21 -0300

    feat: iniciar atividade

--- Atividade 01/app.js
+++ Atividade 01/app.js
@@ -1,5 +1,6 @@
-/**************************************************************
-* Objetivo: Projeto para realizar o cálculo de médias escolares
+/****************************************************************
+* Objetivo: Desenvolver uma aplicação que calcula juros compostos
+* de produtos parcelados para a empresa Viva Moda
 * Autor: Gustavo Vidal de Abreu
 * Data: 04/02/2026 (quarta-feira)
 * Versão: 1.0
@@ -13,3 +14,58 @@ const entradaDeDados = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+entradaDeDados.question("Olá! Somos da empresa Viva Moda. \nDigite o nome do cliente: ", function (nome) {
+    let nomeCliente = nome;
+
+    if (nome == "") {
+        console.log("O campo \"nome\" é obrigatório!")
+    } else if (!isNaN(nome)) {
+        console.log("Não é permitida a entrada de números no campo \"nome\"!")
+    } else {
+        entradaDeDados.question("Qual é o produto comprado? ", function (produto) {
+            let produtoComprado = produto;
+
+            if (produto == "") {
+                console.log("O campo \"produto\" é obrigatório!")
+            } else if (!isNaN(produto)) {
+                console.log("Não é permitida a entrada de números no campo \"produto\"!")
+            } else {
+                entradaDeDados.question(`Qual é o valor do produto ${produtoComprado}? R$ `, function (valor) {
+                    let valorProduto = Number(valor);
+
+                    if (valor == "") {
+                        console.log("O campo \"valor\" é obrigatório!")
+                    } else if (isNaN(valor)) {
+                        console.log("Não é permitida a entrada de caracteres no campo \"valor\"!")
+                    } else {
+                        entradaDeDados.question("Qual a taxa de juros (escreva sem o símbolo de %)? ", function (taxa) {
+                            let taxaJuros = Number(taxa) / 100;
+
+                            if (taxa == "") {
+                                console.log("O campo \"taxa\" é obrigatório!")
+                            } else if (isNaN(taxa)) {
+                                console.log("Não é permitida a entrada de caracteres no campo \"taxa\"!")
+                            } else {
+                                entradaDeDados.question(`Muito obrigado, ${nomeCliente}. O seu tempo de pagamento é em meses ou anos? \n[1] Meses \n[2] Anos \nDigite 1 ou 2: `, function (tempo) {
+                                    let tempoEscolhido = tempo;
+
+                                    entradaDeDados.question("Em quantas vezes o produto foi parcelado? ", function (vezes) {
+                                        let vezesParceladas = Number(vezes);
+                                        if (tempoEscolhido == 2) {
+                                            vezesParceladas = vezesParceladas * 12;
+                                        }
+
+                                        let montante = valorProduto * (1 + taxaJuros) ** vezesParceladas;
+
+                                        console.log(toFixed(montante));
+                                    }); // Fecha quantidade parcelada
+                                }); // Fecha tempo
+                            }
+                        }); // Fecha taxa
+                    }
+                }); // Fecha valor
+            }
+        }); // Fecha produto
+    }
+}); // Fecha nome
