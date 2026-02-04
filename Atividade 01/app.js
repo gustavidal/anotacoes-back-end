2b46c6e9cdcfda3a56f829290cf2ab8b31dbce9c
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-04 15:40:14 -0300

    docs: documentar dados do projeto

---
+++ Atividade 01/app.js
@@ -0,0 +1,15 @@
+/**************************************************************
+* Objetivo: Projeto para realizar o cálculo de médias escolares
+* Autor: Gustavo Vidal de Abreu
+* Data: 04/02/2026 (quarta-feira)
+* Versão: 1.0
+**************************************************************/
+
+// Import da biblioteca de entrada de dados
+const readline = require("readline");
+
+// Criação do objeto que guarda as entradas de dados
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
