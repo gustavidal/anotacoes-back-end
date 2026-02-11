fc20a52aefb5a00193c241a38fecb841d1260c12
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-11 15:03:59 -0300

    feat: adicionar funções

--- Aula 03/app.js
+++ Aula 03/app.js
@@ -16,35 +16,71 @@ const entradaDeDados = readline.createInterface({
 })
 
 // Entrada do nome do cliente
-entradaDeDados.question('Digite o nome do cliente: ', function(nome){
+entradaDeDados.question('Digite o nome do cliente: ', function (nome) {
     let nomeCliente = nome
 
     // Entrada do nome do produto
-    entradaDeDados.question('Digite o nome do produto: ', function(produto){
+    entradaDeDados.question('Digite o nome do produto: ', function (produto) {
         let nomeProduto = produto
 
         // Entrada do valor da compra
-        entradaDeDados.question('Digite o valor da compra: ', function(valor){
+        entradaDeDados.question('Digite o valor da compra: ', function (valor) {
             let valorCompra = valor
 
             // Entrada da taxa de juros
-            entradaDeDados.question('Digite a taxa de juros: ', function(taxa){
+            entradaDeDados.question('Digite a taxa de juros: ', function (taxa) {
                 let taxaJuros = taxa
 
                 // Entrada da quantidade de parcelas
-                entradaDeDados.question('Digite a quantidade de parcelas: ', function(parcelas){
+                entradaDeDados.question('Digite a quantidade de parcelas: ', function (parcelas) {
                     let qtdeParcelas = parcelas
 
-                    // Validações
+                    let montante = calcularJurosCompostos(valorCompra, taxaJuros, qtdeParcelas)
 
-                    // Cálculos
-                    let percentual = Number(taxaJuros) / 100
+                    console.log(montante)
 
-                    let montante = Number(valorCompra) * ((1 + percentual) ** Number(qtdeParcelas))
-
-                    console.log('O valor final é: ' + montante.toFixed(2))
+                    // console.log('O valor final é: ' + montante.toFixed(2))
                 })
             })
         })
     })
 })
+
+// Função para retornar o percentual de um número
+function calcularPercentual(numero) {
+    // Recebe o número encaminhado
+    let numeroPercentual = Number(numero)
+
+    // Validação de entrada vazia, menor ou igual a 0 e de caracter
+    if (numero == '' || numero <= 0 || isNaN(numero)) {
+        return false
+    } else {
+        // Calcula o percentual do número
+        let percentual = numeroPercentual / 100
+        return Number(percentual.toFixed(2))
+    }
+}
+
+// Função para retornar o montante referente a juros compostos
+function calcularJurosCompostos(valor, taxa, parcelas) {
+    // Recebe os valores dos argumentos e converte em número
+    let valorPrincipal = Number(valor)
+    let taxaJuros = Number(taxa)
+    let qtdeParcelas = Number(parcelas)
+
+    // Validação de vazio e de caracteres
+    if (valor == '' || isNaN(valor) || valor <= 0 || parcelas == '' || isNaN(parcelas) || parcelas <= 0) {
+        return false
+    } else {
+        // Chama a função para retornar o percentual da taxa
+        let percentual = calcularPercentual(taxaJuros)
+
+        if (percentual) {
+            // Cálculo
+            let montante = valorPrincipal * ((1 + percentual) ** qtdeParcelas)
+            return Number(montante.toFixed(2))
+        } else {
+            return false
+        }
+    }
+}
