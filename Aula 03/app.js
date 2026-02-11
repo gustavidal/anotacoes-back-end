51e0b8410f02d3e2a63cdf29e5832c2f27124859
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-11 15:54:09 -0300

    feat: concluir projeto

--- Aula 03/app.js
+++ Aula 03/app.js
@@ -35,8 +35,11 @@ entradaDeDados.question('Digite o nome do cliente: ', function (nome) {
                 entradaDeDados.question('Digite a quantidade de parcelas: ', function (parcelas) {
                     let qtdeParcelas = parcelas
 
+                    // Import da biblioteca de cálculos financeiros
+                    let calculos = require('./modulo/calculos.js')
+
                     // Chama a função para calcular o valor do montante
-                    let montante = calcularJurosCompostos(valorCompra, taxaJuros, qtdeParcelas)
+                    let montante = calculos.calcularJurosCompostos(valorCompra, taxaJuros, qtdeParcelas)
 
                     // Valida se o cálculo foi realizado
                     if (montante) {
@@ -50,42 +53,3 @@ entradaDeDados.question('Digite o nome do cliente: ', function (nome) {
         })
     })
 })
-
-// Função para retornar o percentual de um número
-function calcularPercentual(numero) {
-    // Recebe o número encaminhado
-    let numeroPercentual = Number(numero)
-
-    // Validação de entrada vazia, menor ou igual a 0 e de caracter
-    if (numero == '' || numero <= 0 || isNaN(numero)) {
-        return false
-    } else {
-        // Calcula o percentual do número
-        let percentual = numeroPercentual / 100
-        return Number(percentual.toFixed(2))
-    }
-}
-
-// Função para retornar o montante referente a juros compostos
-function calcularJurosCompostos(valor, taxa, parcelas) {
-    // Recebe os valores dos argumentos e converte em número
-    let valorPrincipal = Number(valor)
-    let taxaJuros = Number(taxa)
-    let qtdeParcelas = Number(parcelas)
-
-    // Validação de vazio e de caracteres
-    if (valor == '' || isNaN(valor) || valor <= 0 || parcelas == '' || isNaN(parcelas) || parcelas <= 0) {
-        return false
-    } else {
-        // Chama a função para retornar o percentual da taxa
-        let percentual = calcularPercentual(taxaJuros)
-
-        if (percentual) {
-            // Cálculo
-            let montante = valorPrincipal * ((1 + percentual) ** qtdeParcelas)
-            return Number(montante.toFixed(2))
-        } else {
-            return false
-        }
-    }
-}
---
+++ Aula 03/folhapagto.js
@@ -0,0 +1,8 @@
+let acrescimo = 5
+
+// Import da biblioteca de cálculos financeiros
+let calculos = require('./modulo/calculos.js')
+
+let percentual = calculos.calcularPercentual(acrescimo)
+
+console.log(percentual)
