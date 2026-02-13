dfb45832cec8086240e1b9370256e9456b3b9a70
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2026-02-13 19:49:42 -0300

    fix: melhorar código

--- Atividade 02/modulo/calculos.js
+++ Atividade 02/modulo/calculos.js
@@ -10,17 +10,14 @@ function definirOperacao(opcao) {
     let opcaoOperacao = opcao
     let operacao
 
-    if (opcao === '1' || opcao === '2' ||
-        opcao === '3' || opcao === '4') {
-        if (opcaoOperacao == 1) {
-            return operacao = 'ADIÇÃO'
-        } else if (opcaoOperacao == 2) {
-            return operacao = 'SUBTRAÇÃO'
-        } else if (opcaoOperacao == 3) {
-            return operacao = 'MULTIPLICAÇÃO'
-        } else {
-            return operacao = 'DIVISÃO'
-        }
+    if (opcaoOperacao === '1') {
+        return operacao = 'ADIÇÃO'
+    } else if (opcaoOperacao === '2') {
+        return operacao = 'SUBTRAÇÃO'
+    } else if (opcaoOperacao === '3') {
+        return operacao = 'MULTIPLICAÇÃO'
+    } else if (opcaoOperacao === '4') {
+        return operacao = 'DIVISÃO'
     } else {
         return false
     }
@@ -31,11 +28,11 @@ function definirNomeAoNum1(operacao) {
     let tipoOperacao = operacao
     let nomeNum1
 
-    if (tipoOperacao == 'ADIÇÃO') {
+    if (tipoOperacao === 'ADIÇÃO') {
         return nomeNum1 = 'a 1ª parcela'
-    } else if (tipoOperacao == 'SUBTRAÇÃO') {
+    } else if (tipoOperacao === 'SUBTRAÇÃO') {
         return nomeNum1 = 'o minuendo'
-    } else if (tipoOperacao == 'MULTIPLICAÇÃO') {
+    } else if (tipoOperacao === 'MULTIPLICAÇÃO') {
         return nomeNum1 = 'o 1º fator'
     } else {
         return nomeNum1 = 'o dividendo'
@@ -47,11 +44,11 @@ function definirNomeAoNum2(operacao) {
     let tipoOperacao = operacao
     let nomeNum2
 
-    if (tipoOperacao == 'ADIÇÃO') {
+    if (tipoOperacao === 'ADIÇÃO') {
         return nomeNum2 = 'a 2ª parcela'
-    } else if (tipoOperacao == 'SUBTRAÇÃO') {
+    } else if (tipoOperacao === 'SUBTRAÇÃO') {
         return nomeNum2 = 'o subtraendo'
-    } else if (tipoOperacao == 'MULTIPLICAÇÃO') {
+    } else if (tipoOperacao === 'MULTIPLICAÇÃO') {
         return nomeNum2 = 'o 2º fator'
     } else {
         return nomeNum2 = 'o divisor'
@@ -63,11 +60,11 @@ function definirNomeAoResultado(operacao) {
     let tipoOperacao = operacao
     let nomeResultado
 
-    if (tipoOperacao == 'ADIÇÃO') {
+    if (tipoOperacao === 'ADIÇÃO') {
         return nomeResultado = 'Soma'
-    } else if (tipoOperacao == 'SUBTRAÇÃO') {
+    } else if (tipoOperacao === 'SUBTRAÇÃO') {
         return nomeResultado = 'Diferença'
-    } else if (tipoOperacao == 'MULTIPLICAÇÃO') {
+    } else if (tipoOperacao === 'MULTIPLICAÇÃO') {
         return nomeResultado = 'Produto'
     } else {
         return nomeResultado = 'Quociente'
@@ -78,7 +75,7 @@ function definirNomeAoResultado(operacao) {
 function validarEntradaValor1(valor) {
     let valor1 = Number(valor.replace(',', '.'))
 
-    if (valor == '' || isNaN(valor1)) {
+    if (valor.trim() === '' || isNaN(valor1)) {
         return null
     } else {
         return valor1
@@ -89,11 +86,11 @@ function validarEntradaValor1(valor) {
 function validarEntradaValor2(valor) {
     let valor2 = Number(valor.replace(',', '.'))
 
-    if (valor == '' || isNaN(valor2)) {
+    if (valor.trim() === '' || isNaN(valor2)) {
         return null
+    } else {
+        return valor2
     }
-
-    return valor2
 }
 
 // Função para retornar o resultado da operação escolhida
@@ -103,11 +100,11 @@ function calcularResultado(operacao, numero1, numero2) {
     let valor2 = numero2
     let resultado
 
-    if (tipoOperacao == 'ADIÇÃO') {
+    if (tipoOperacao === 'ADIÇÃO') {
         resultado = valor1 + valor2
-    } else if (tipoOperacao == 'SUBTRAÇÃO') {
+    } else if (tipoOperacao === 'SUBTRAÇÃO') {
         resultado = valor1 - valor2
-    } else if (tipoOperacao == 'MULTIPLICAÇÃO') {
+    } else if (tipoOperacao === 'MULTIPLICAÇÃO') {
         resultado = valor1 * valor2
     } else {
         resultado = valor1 / valor2
