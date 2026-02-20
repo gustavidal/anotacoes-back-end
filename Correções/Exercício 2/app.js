5dfece7557ec96f15b91fafda36385256e44818d
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-20 16:56:27 -0300

    feat: adicionar correção
    docs: adicionar conteúdo

---
+++ Correções/Exercício 2/app.js
@@ -0,0 +1,6 @@
+/****************************************************************************
+ * Objetivo: Arquivo responsável SOMENTE pela entrada e saída de dados
+ * Data: 20/02/2026
+ * Autor: Gustavo Vidal de Abreu
+ * Versão: 1.0
+ ***************************************************************************/
--- Correções/Exercício 2/modulo/calculo.js
+++ Correções/Exercício 2/modulo/calculo.js
@@ -4,4 +4,50 @@
  * Data: 20/02/2026
  * Autor: Gustavo Vidal de Abreu
  * Versão: 1.0
- ***************************************************************************/
+****************************************************************************/
+
+/*
+ * toLowerCase() -> retorna uma string em minúsculo
+ * toUpperCase() -> RETORNA UMA STRING EM MAIÚSCULO
+*/
+
+// Exemplo de função anônima
+// Função para calcular as 4 operações matemáticas
+const calcular = function (numero1, numero2, operador) {
+    // Entrada de dados
+    let valor1 = Number(numero1)
+    let valor2 = Number(numero2)
+    let operadorMatematico = String(operador).toUpperCase()
+
+    let resultado
+
+    // Processamento
+    // if (operadorMatematico == 'SOMAR') {
+    //     resultado = valor1 + valor2
+    // } else if (operadorMatematico == 'SUBTRAIR') {
+    //     resultado = valor1 - valor2
+    // } else if (operadorMatematico == 'MULTIPLICAR') {
+    //     resultado = valor1 * valor2
+    // } else if (operadorMatematico == 'DIVIDIR') {
+    //     resultado = valor1 / valor2
+    // }
+
+    switch (operadorMatematico) {
+        case 'SOMAR':       resultado = valor1 + valor2; break;
+        case 'SUBTRAIR':    resultado = valor1 - valor2; break;
+        case 'MULTIPLICAR': resultado = valor1 * valor2; break;
+        case 'DIVIDIR':     resultado = valor1 / valor2; break;
+    }
+
+    // Saída
+    if (resultado != undefined) { return resultado } else { return false }
+}
+
+let result = calcular(13, 665, 'somar')
+
+console.log(result)
+// if (result) {
+//     console.log(result)
+// } else {
+//     console.log('ERRO')
+// }
