e2a0ce10100fb5797a6b15b41168f985ae4c4178
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-20 15:33:46 -0300

    feat: adicionar pasta de correções
    refactor: refatorar posicionamento de arquivos
    
    docs: documentar README

---
+++ Correções/Exercício 2/modulo/calculo.js
@@ -0,0 +1,7 @@
+/****************************************************************************
+ * Objetivo: Arquivo responsável pelo processamento de cáculos matemáticos
+ * básicos (adição, subtração, multiplicação e divisão)
+ * Data: 20/02/2026
+ * Autor: Gustavo Vidal de Abreu
+ * Versão: 1.0
+ ***************************************************************************/
