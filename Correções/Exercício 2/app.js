a7f5b5b9866345d7c7f21468f5d92f95ab6f8dbd
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-25 15:00:55 -0300

    feat: finalizar correção

--- Correções/Exercício 2/app.js
+++ Correções/Exercício 2/app.js
@@ -4,3 +4,21 @@
  * Autor: Gustavo Vidal de Abreu
  * Versão: 1.0
  ***************************************************************************/
+
+// Import da biblioteca de cálculos
+const calculosMatematicos = require('./modulo/calculo.js')
+
+let n1 = '10,5'
+let n2 = 20
+let operador = 'somar'
+
+let validar = calculosMatematicos.validarDados(n1, n2, operador)
+
+if (validar) {
+    let result = calculosMatematicos.calcular(80, 55, operador)
+    if (result)
+        console.log(result)
+    else
+        console.log('ERRO: Não foi possível fazer o cálculo!')
+} else
+    console.log('ERRO: Validação de dados incorreta!')
--- Correções/Exercício 2/modulo/calculo.js
+++ Correções/Exercício 2/modulo/calculo.js
@@ -11,6 +11,18 @@
  * toUpperCase() -> RETORNA UMA STRING EM MAIÚSCULO
 */
 
+const validarDados = function (numero1, numero2, operador) {
+    let valor1 = Number(numero1)
+    let valor2 = Number(numero2)
+    let operadorMatematico = String(operador)
+
+    if (numero1 == '' || isNaN(numero1) || numero2 == '' || isNaN(numero2) || operador == '') {
+        return false
+    } else {
+        return true
+    }
+}
+
 // Exemplo de função anônima
 // Função para calcular as 4 operações matemáticas
 const calcular = function (numero1, numero2, operador) {
@@ -22,32 +34,37 @@ const calcular = function (numero1, numero2, operador) {
     let resultado
 
     // Processamento
-    // if (operadorMatematico == 'SOMAR') {
+    // if (operadorMatematico == 'SOMAR')
     //     resultado = valor1 + valor2
-    // } else if (operadorMatematico == 'SUBTRAIR') {
+    // else if (operadorMatematico == 'SUBTRAIR')
     //     resultado = valor1 - valor2
-    // } else if (operadorMatematico == 'MULTIPLICAR') {
+    // else if (operadorMatematico == 'MULTIPLICAR')
     //     resultado = valor1 * valor2
-    // } else if (operadorMatematico == 'DIVIDIR') {
+    // else if (operadorMatematico == 'DIVIDIR')
     //     resultado = valor1 / valor2
-    // }
 
     switch (operadorMatematico) {
-        case 'SOMAR':       resultado = valor1 + valor2; break;
-        case 'SUBTRAIR':    resultado = valor1 - valor2; break;
-        case 'MULTIPLICAR': resultado = valor1 * valor2; break;
-        case 'DIVIDIR':     resultado = valor1 / valor2; break;
+        case 'SOMAR': resultado = somar(valor1, valor2); break;
+        case 'SUBTRAIR': resultado = subtrair(valor1, valor2); break;
+        case 'MULTIPLICAR': resultado = multiplicar(valor1, valor2); break;
+        case 'DIVIDIR': resultado = dividir(valor1, valor2); break;
     }
 
     // Saída
     if (resultado != undefined) { return resultado } else { return false }
 }
 
-let result = calcular(13, 665, 'somar')
+// Exemplo de função em seta (arrow function)
+const somar = (numero1, numero2) => Number(numero1) + Number(numero2)
+const subtrair = (numero1, numero2) => Number(numero1) - Number(numero2)
+const multiplicar = (numero1, numero2) => Number(numero1) * Number(numero2)
+const dividir = (numero1, numero2) => Number(numero1) / Number(numero2)
 
-console.log(result)
-// if (result) {
-//     console.log(result)
-// } else {
-//     console.log('ERRO')
-// }
+module.exports = {
+    calcular,
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    validarDados
+}
