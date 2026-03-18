745b7e9537814d289fba95bba97807be24ba74dd
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-03-18 16:58:53 -0300

    wip: iniciar função de descrição de estado específico

--- Atividade 04/modulo/funcoes.js
+++ Atividade 04/modulo/funcoes.js
@@ -7,6 +7,9 @@
 
 let arquivo = require('./estados_cidades.js')
 
+const listaDeEstados = arquivo.listaDeEstados
+const ESTADOS = listaDeEstados.estados
+
 // Função que retorna uma lista com todas as siglas dos estados brasileiros junto com a quantidade de siglas
 const getListaDeEstados = function () {
     // Criação de variáveis
@@ -15,7 +18,7 @@ const getListaDeEstados = function () {
     let quantidade
 
     // Loop que lista todos os estados e seus atributos (nome, sigla, cidades e etc)
-    arquivo.listaDeEstados.estados.forEach(function (itemLista) {
+    ESTADOS.forEach(function (itemLista) {
         // Adiciona TODAS e SOMENTE as siglas dos estados brasileiros
         uf.push(itemLista.sigla)
     })
@@ -31,4 +34,14 @@ const getListaDeEstados = function () {
     return lista
 }
 
-getListaDeEstados()
+const getDadosEstados = function (siglaEstado) {
+    let lista = {}
+    let siglas = getListaDeEstados()
+
+    siglas.uf.forEach(function (itemLista) {
+        console.log(itemLista)
+    })
+}
+
+// getListaDeEstados()
+getDadosEstados('SP')
