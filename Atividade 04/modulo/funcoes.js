47b1bc265d0b58c24b8370d42421591f3b875ab0
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-03-20 15:59:08 -0300

    docs: documentar funções

--- Atividade 04/modulo/funcoes.js
+++ Atividade 04/modulo/funcoes.js
@@ -80,7 +80,7 @@ const getEstadosRegiao = function (regiaoEstados) {
         if (estado.regiao.toUpperCase() === regiaoEstados.toUpperCase()) {
             // Adiciona o estado a lista de resposta
             lista.estados.push({
-                "uf": estado.sigla,
+                "uf":        estado.sigla,
                 "descricao": estado.nome
             })
         }
@@ -100,17 +100,18 @@ const getCapitalPais = function () {
     }
 
     // Percorre o arquivo
-    ESTADOS.forEach(function (estado) {
+    ESTADOS.forEach(function (itemEstado) {
         // Caso algum estado tenha o atributo de capital do país
-        if (estado.capital_pais) {
+        if (itemEstado.capital_pais) {
             // Adiciona as informações ao array das capitais
             lista.capitais.push({
-                "capital_atual": estado.capital_pais.capital,
-                "uf": estado.sigla,
-                "descricao": estado.nome,
-                "capital": estado.capital,
-                "capital_pais_ano_inicio": estado.capital_pais.ano_inicio,
-                "capital_pais_ano_termino": estado.capital_pais.ano_fim
+                "capital_atual":            itemEstado.capital_pais.capital,
+                "uf":                       itemEstado.sigla,
+                "descricao":                itemEstado.nome,
+                "capital":                  itemEstado.capital,
+                "regiao":                   itemEstado.regiao,
+                "capital_pais_ano_inicio":  itemEstado.capital_pais.ano_inicio,
+                "capital_pais_ano_termino": itemEstado.capital_pais.ano_fim
             })
         }
     })
@@ -118,6 +119,7 @@ const getCapitalPais = function () {
     return lista
 }
 
+// Função que retorna todas as cidades de um estado
 const getCidades = function (siglaEstado) {
     let lista = {
         "uf": siglaEstado.toUpperCase(),
@@ -126,20 +128,26 @@ const getCidades = function (siglaEstado) {
         "cidades": []
     }
 
+    // Percorre os estados
     for (let estado of ESTADOS) {
+        // Caso as siglas forem iguais
         if (estado.sigla.toUpperCase() === siglaEstado.toUpperCase()) {
             lista.descricao = estado.nome
-            lista.quantidade
+
+            // Percorre as cidades do estado informado
             estado.cidades.forEach(function (itemCidade) {
+                // Adiciona as cidades ao array
                 lista.cidades.push(itemCidade.nome)
             })
-            lista.quantidade = lista.cidades.length
         }
-
     }
-
+    
+    // Valida caso não foi encontrado nenhuma cidade
     if (lista.cidades.length == 0)
         return false
+    
+    // Define a quantidade de cidades do estado
+    lista.quantidade = lista.cidades.length
 
     return lista
 }
