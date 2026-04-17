ecacbab274bfb6de165c3947de6f7d06e8a87509
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2026-04-17 20:16:07 -0300

    fix: corrigir length

--- AULAS/Aula 06/controller/filme/controller_filme.js
+++ AULAS/Aula 06/controller/filme/controller_filme.js
@@ -16,19 +16,19 @@ const inserirNovoFilme = async function (filme) {
     // Cria uma cópia dos JSON's do arquivo de configurações de mensagens
     let customMessages = JSON.parse(JSON.stringify(configMessages))
 
-    if (filme.nome == '' || filme.nome == null || filme.nome == undefined || filme.nome.lenght > 80) {
+    if (filme.nome == '' || filme.nome == null || filme.nome == undefined || filme.nome.length > 80) {
         customMessages.ERROR_BAD_REQUEST.field = '[NOME] INVÁLIDO'
     } else if (filme.sinopse == '' || filme.sinopse == null || filme.sinopse == undefined) {
         customMessages.ERROR_BAD_REQUEST.field = '[SINOPSE] INVÁLIDA'
-    } else if (filme.capa == '' || filme.capa == null || filme.capa == undefined || filme.capa.lenght > 255) {
+    } else if (filme.capa == '' || filme.capa == null || filme.capa == undefined || filme.capa.length > 255) {
         customMessages.ERROR_BAD_REQUEST.field = '[CAPA] INVÁLIDA'
-    } else if (filme.data_lancamento == '' || filme.data_lancamento == null || filme.data_lancamento == undefined || filme.data_lancamento.lenght != 10) {
+    } else if (filme.data_lancamento == '' || filme.data_lancamento == null || filme.data_lancamento == undefined || filme.data_lancamento.length != 10) {
         customMessages.ERROR_BAD_REQUEST.field = '[DATA DE LANÇAMENTO] INVÁLIDA'
-    } else if (filme.duracao == '' || filme.duracao == null || filme.duracao == undefined || filme.duracao.lenght < 5) {
+    } else if (filme.duracao == '' || filme.duracao == null || filme.duracao == undefined || filme.duracao.length < 5) {
         customMessages.ERROR_BAD_REQUEST.field = '[DURAÇÃO] INVÁLIDA'
-    } else if (filme.valor == undefined || isNaN(filme.valor) || filme.valor.lenght > 5) {
+    } else if (filme.valor == undefined || isNaN(filme.valor) || filme.valor.length > 5) {
         customMessages.ERROR_BAD_REQUEST.field = '[VALOR] INVÁLIDO'
-    } else if (filme.avaliacao == undefined || isNaN(filme.avaliacao) || filme.avaliacao.lenght > 3) {
+    } else if (filme.avaliacao == undefined || isNaN(filme.avaliacao) || filme.avaliacao.length > 3) {
         customMessages.ERROR_BAD_REQUEST.field = '[AVALIAÇÃO] INVÁLIDA'
     } else {
         let result = await filmeDAO.insertFilme(filme)
